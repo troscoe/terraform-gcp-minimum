abb1c8e9be352302abe1cfd12476b8247d6cbb7f
Tristan Roscoe <troscoe@gmail.com>
2020-03-04 15:38:36 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "google_compute_image" "fortipoc" {
 
 resource "google_compute_instance" "default" {
   name         = join("-", ["fortipoc",var.username,var.enrol-id])
-  machine_type = "n1-standard-2"
+  machine_type = "n1-standard-8"
   zone         = "us-east4-a"
 
   tags = ["http-server", "https-server"]
