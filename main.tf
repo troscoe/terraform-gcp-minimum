c7089050ecdf1bb82da446edd6cfae411a044a8c
Tristan Roscoe <troscoe@gmail.com>
2020-03-06 03:56:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -63,21 +63,9 @@ resource "google_compute_instance" "default" {
   }
   provisioner "local-exec" {
     command = <<EOH
-curl \
--H 'Content-Type: application/json' \
--c cookies.txt -b cookies.txt \
--k https://${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}/api/v0/login
--d '{"username":"admin","password":"${var.sshpassword}"}' \
-
+curl -H 'Content-Type: application/json' -c cookies.txt -b cookies.txt -k https://${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}/api/v0/logi-d '{"username":"admin","password":"${var.sshpassword}"}' \
 srftoken=`grep csrftoken cookies.txt | cut -f 7`
-
-curl \
--H "X-Fortipoc-Csrftoken: $srftoken" \
--H 'Content-Type: application/json' \
--c cookies.txt -b cookies.txt \
--e https://${google_compute_instance.default.network_interface.0.access_config.0.nat_ip} \
--k https://${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}/api/v0/poc/launch \
--d '{"poc":"1"}'
+curl -H "X-Fortipoc-Csrftoken: $srftoken" -H 'Content-Type: application/json' -c cookies.txt -b cookies.txt -e https://${google_compute_instance.default.network_interface.0.access_config.0.nat_ip} -k https://${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}/api/v0/poc/launch -d '{"poc":"1"}'
 EOH
   }
 }
