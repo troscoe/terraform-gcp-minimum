bd5720ac31213edffa94b19aacec89b1539bb277
Tristan Roscoe <troscoe@gmail.com>
2020-03-06 04:45:37 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,6 +24,10 @@ resource "google_service_account_key" "mykey" {
   service_account_id = data.google_service_account.myaccount.name
 }
 
+resource "google_compute_address" "static" {
+  name = "ipv4-address"
+}
+
 resource "google_compute_instance" "default" {
   name         = join("-", ["fortipoc",var.username,var.enrol-id])
   machine_type = "n1-standard-16"
@@ -43,9 +47,8 @@ resource "google_compute_instance" "default" {
 
   network_interface {
     network = "default"
-
     access_config {
-      // Ephemeral IP
+      nat_ip = google_compute_address.static.address
     }
   }
 
@@ -55,7 +58,7 @@ resource "google_compute_instance" "default" {
   
   provisioner "remote-exec" {
     connection {
-      host        = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
+      host        = google_compute_address.static.address
       type        = "ssh"
       user        = "admin"
       password    = var.sshpassword
@@ -63,14 +66,14 @@ resource "google_compute_instance" "default" {
   }
   provisioner "local-exec" {
     command = <<EOH
-curl -H 'Content-Type: application/json' -c cookies.txt -b cookies.txt -k https://${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}/api/v0/login -d '{"username":"admin","password":"${var.sshpassword}"}'
+curl -H 'Content-Type: application/json' -c cookies.txt -b cookies.txt -k https://${google_compute_address.static.address}/api/v0/login -d '{"username":"admin","password":"${var.sshpassword}"}'
 srftoken=`grep csrftoken cookies.txt | cut -f 7`
 sleep 10
-curl -H "X-Fortipoc-Csrftoken: $srftoken" -H 'Content-Type: application/json' -c cookies.txt -b cookies.txt -e https://${google_compute_instance.default.network_interface.0.access_config.0.nat_ip} -k https://${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}/api/v0/poc/launch -d '{"poc":"1"}'
+curl -H "X-Fortipoc-Csrftoken: $srftoken" -H 'Content-Type: application/json' -c cookies.txt -b cookies.txt -e https://${google_compute_address.static.address} -k https://${google_compute_address.static.address}/api/v0/poc/launch -d '{"poc":"1"}'
 EOH
   }
 }
 
 output "instance_ip_addr" {
-  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
+  value = google_compute_address.static.address
 }
