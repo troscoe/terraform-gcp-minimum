077c8725d03ba84580d5838975d86ea8a200435e
Tristan Roscoe <troscoe@gmail.com>
2020-03-06 04:55:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "google_compute_instance" "default" {
   network_interface {
     network = "default"
     access_config {
-      nat_ip = google_compute_address.static.address
+      // Ephemeral IP
     }
   }
 
