9a5006f3fb7cedf291ae7e1cf4e7be15e35d7ce7
Tristan Roscoe <troscoe@gmail.com>
2020-03-06 04:50:44 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -74,6 +74,45 @@ EOH
   }
 }
 
+resource "null_resource" "start_instance" {
+  provisioner "local-exec" {
+    command     = <<EOH
+export PATH="/terraform/google-cloud-sdk/bin:$PATH"
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-283.0.0-linux-x86_64.tar.gz
+tar -xf google-cloud-sdk-283.0.0-linux-x86_64.tar.gz
+cd google-cloud-sdk
+./install.sh
+cat >> /terraform/google-cloud-sdk/key.json <<EOL
+${base64decode(google_service_account_key.mykey.private_key)}
+EOL
+gcloud auth activate-service-account --key-file=/terraform/google-cloud-sdk/key.json
+gcloud compute instances start ${google_compute_instance.default.name} --zone ${google_compute_instance.default.zone}
+EOH
+  }
+  
+  provisioner "remote-exec" {
+    connection {
+      host        = google_compute_address.static.address
+      type        = "ssh"
+      user        = "admin"
+      password    = var.sshpassword
+    }
+  }
+  
+  provisioner "local-exec" {
+    command = <<EOH
+curl -H 'Content-Type: application/json' -c cookies.txt -b cookies.txt -k https://${google_compute_address.static.address}/api/v0/login -d '{"username":"admin","password":"${var.sshpassword}"}'
+srftoken=`grep csrftoken cookies.txt | cut -f 7`
+sleep 10
+curl -H "X-Fortipoc-Csrftoken: $srftoken" -H 'Content-Type: application/json' -c cookies.txt -b cookies.txt -e https://${google_compute_address.static.address} -k https://${google_compute_address.static.address}/api/v0/poc/current/devices/poweron -d '{"devices":[1,2,3,4,5,6,7,8,9,10,11]}'
+EOH
+  }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
 output "instance_ip_addr" {
   value = google_compute_address.static.address
 }
